89180563178e09842713a0db26b7dcb5e501c546
Colson Xu <colson.xu@gmail.com>
2022-05-20 18:13:46 -0400

    optimized two sum even more

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -1,11 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        count = {num: 0 for num in nums}
-        for num in nums:
-            count[num] += 1
-
-        for i in range(len(nums)):
-            complement = target - nums[i]
-            if complement in count.keys() and count[complement] >= 1 and not nums.index(complement) == i:
-                return [i, nums.index(complement)]
+        count = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in count.keys():
+                return [i, count[complement]]
+            count[num] = i
 
