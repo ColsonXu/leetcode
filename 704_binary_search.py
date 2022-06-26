ddbd802ab9a6e1e15c521018f565127188cf5efc
Colson Xu <colson.xu@gmail.com>
2022-06-26 12:12:02 -0400

    704

--- 704_binary_search.py
+++ 704_binary_search.py
@@ -6,9 +6,9 @@ class Solution:
         while l < r:
             mid = (l + r) // 2
             if nums[mid] < target:
-                l = mid
+                l = mid + 1
             elif nums[mid] > target:
-                r = mid
+                r = mid - 1
             else:
                 return mid
         return -1
