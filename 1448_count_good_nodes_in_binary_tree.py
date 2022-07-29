3f228618a4b89d7d51965f0e325d92b0c09eb692
Colson Xu <colson.xu@gmail.com>
2022-07-29 11:11:31 -0400

    1448

--- 1448_count_good_nodes_in_binary_tree.py
+++ 1448_count_good_nodes_in_binary_tree.py
@@ -1,5 +1,14 @@
 class Solution:
     def goodNodes(self, root):
-        stack = [root]
-        max = -9999999999
-        
+        def dfs(s, max_val):
+            if not s:
+                return 0
+
+            res = 1 if s.val >= max_val else 0
+            max_val = max(max_val, s.val)
+            res += dfs(s.left, max_val)
+            res += dfs(s.right, max_val)
+            
+            return res
+
+        return dfs(root, root.val)
