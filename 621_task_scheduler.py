ffb55199191e1cfcfd7ace0830af48cf046f522b
Colson Xu <colson.xu@gmail.com>
2022-08-03 15:22:23 -0400

    621

--- 621_task_scheduler.py
+++ 621_task_scheduler.py
@@ -1,3 +1,24 @@
+from collections import Counter, deque
+import heapq
+
 class Solution:
     def leastInterval(self, tasks, n):
+        count = Counter(tasks)
+        heap = [-x for x in count.values()]
+        heapq.heapify(heap)
         
+        q = deque()
+        time = 0
+
+        while heap or q:
+            time += 1
+
+            if not heap:
+                time = q[0][1]
+            else:
+                cnt = 1 + heapq.heappop(heap)
+                if cnt:
+                    q.append([cnt, time + n])
+            if q and q[0][1] == time:
+                heapq.heappush(heap, q.popleft()[0])
+        return time
