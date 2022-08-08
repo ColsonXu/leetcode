09b29e4fc18a4a38f12aa46ecd516c844752087e
Colson Xu <colson.xu@gmail.com>
2022-08-08 10:02:42 -0400

    90

--- 90_subsets_ii.py
+++ 90_subsets_ii.py
@@ -1,3 +1,20 @@
 class Solution:
     def subsetsWithDup(self, nums):
+        res = []
+        nums.sort()
 
+        def backtrack(i, subset):
+            if i >= len(nums):
+                res.append(subset[:])
+                return
+
+            subset.append(nums[i])
+            backtrack(i + 1, subset)
+
+            subset.pop()
+            while i + 1 < len(nums) and nums[i + 1] == nums[i]:
+                i += 1
+            backtrack(i + 1, subset)
+
+        backtrack(0, [])
+        return res
